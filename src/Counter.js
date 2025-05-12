071f63b0da5fdc5d97885140385dbfb3994c04b4
Mohamed Magoor <mohamedmagoor6@gmail.com>
2025-05-12 17:43:01 +0300

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -20,3 +20,38 @@ const Counter = () => {
 };
 
 export default Counter;
+//class based ....
+// import React, { Component } from "react";
+// import "./Counter.css";
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   increment = () => {
+//     this.setState((prevState) => ({ count: prevState.count + 1 }));
+//   };
+
+//   decrement = () => {
+//     this.setState((prevState) => ({ count: prevState.count - 1 }));
+//   };
+
+//   render() {
+//     return (
+//       <div className="counter-container">
+//         <h1>Counter</h1>
+//         <p className="count-display">{this.state.count}</p>
+//         <div className="button-group">
+//           <button onClick={this.increment} className="btn increment">+</button>
+//           <button onClick={this.decrement} className="btn decrement">-</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
